78f95dd5d33ce9d6697dab7d86d5b8c72fb86962
yojeremiah <jschultzdev@gmail.com>
2019-08-20 21:49:02 -0700

    Add DOM elements

---
+++ bootstrap/app.js
@@ -0,0 +1,40 @@
+// DOM ELEMENTS
+// INPUTS
+const metric = document.querySelector('#metric');
+const imperial = document.querySelector('#imperial');
+const age = document.querySelector('#age');
+const height = document.querySelector('#height');
+const weight = document.querySelector('#weight');
+const male = document.querySelector('#male');
+const female = document.querySelector('#female');
+const submit = document.querySelector('#btn-submit');
+// OUTPUTS
+// Cutting
+const cuttingCalsMobile = document.querySelector('#cutting-calories-mobile');
+const cuttingProteinMobile = document.querySelector('#cutting-protein-mobile');
+const cuttingCarbsMobile = document.querySelector('#cutting-carbs-mobile');
+const cuttingFatsMobile = document.querySelector('#cutting-fats-mobile');
+const cuttingCals = document.querySelector('#cutting-calories');
+const cuttingProtein = document.querySelector('#cutting-protein');
+const cuttingCarbs = document.querySelector('#cutting-carbs');
+const cuttingFats = document.querySelector('#cutting-fats');
+// Maintenance
+const maintainCalsMobile = document.querySelector('#maintain-calories-mobile');
+const maintainProteinMobile = document.querySelector(
+  '#maintain-protein-mobile'
+);
+const maintainCarbsMobile = document.querySelector('#maintain-carbs-mobile');
+const maintainFatsMobile = document.querySelector('#maintain-fats-mobile');
+const maintainCals = document.querySelector('#maintain-calories');
+const maintainProtein = document.querySelector('#maintain-protein');
+const maintainCarbs = document.querySelector('#maintain-carbs');
+const maintainFats = document.querySelector('#maintain-fats');
+// Bulking
+const bulkingCalsMobile = document.querySelector('#bulking-calories-mobile');
+const bulkingProteinMobile = document.querySelector('#bulking-protein-mobile');
+const bulkingCarbsMobile = document.querySelector('#bulking-carbs-mobile');
+const bulkingFatsMobile = document.querySelector('#bulking-fats-mobile');
+const bulkingCals = document.querySelector('#bulking-calories');
+const bulkingProtein = document.querySelector('#bulking-protein');
+const bulkingCarbs = document.querySelector('#bulking-carbs');
+const bulkingFats = document.querySelector('#bulking-fats');
